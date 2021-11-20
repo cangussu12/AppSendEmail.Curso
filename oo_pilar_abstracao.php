9480aa56e627b509db57057ddc1d338085e4bead
cangussu12 <leo.tywin@gmail.com>
2021-11-19 21:37:11 -0300

    Aula: pilar do polimorfismo 1

--- oo_pilar_abstracao.php
+++ oo_pilar_abstracao.php
@@ -1,66 +1,39 @@
 <?php
 
-       class Carro extends Veiculo {
-           public $teto_solar = true;
+    class Pai {
+        private $nome = 'jorge';
+        protected $sobrenome = 'Silva';
+        public $humor = 'Feliz';
 
-           function __construct($placa, $cor) {
-               $this->placa = $placa;
-               $this->cor = $cor;
-           }
-       
-           function abrirTetoSolar() {
-            echo 'Abrir teto solar';
+
+        public function __get($attr) {
+            return $this->$attr;
         }
 
-        
-        function alterarPosicaoVolante() {
-            echo 'Alterar posição do volante';
-        }
-       }
-
-       class Moto extends Veiculo{
-           public $contraPesoGuidao = true;
-
-           function __construct($placa, $cor) {
-            $this->placa = $placa;
-            $this->cor = $cor;
+        public function __set($attr, $value) {
+            $this->$attr = $value;
         }
 
-           function empinar(){
-            echo 'Empinar';
+        private function executarMania() {
+            echo 'Assobiar';
         }
 
-        function trocarMarcha() {
-            echo 'Desengatar embreagem com a mão e engatar marcha com o pé';
-        }
-       }
-
-       class Veiculo {
-           public $placa = null;
-           public $cor = null;
-
-           function acelerar(){
-               echo 'Acelerar';
-           }
-
-           function freiar(){
-            echo 'Freiar';
+        protected function responder() {
+            echo ' Oi';
         }
 
-        function trocarMarcha() {
-            echo 'Desengatar embreagem com o pe e engatar marcha com a mão';
-        }
-       }
+        public function executarAcao() {
+            $x = rand(1, 10);
 
-       class Caminhao extends Veiculo {}
-
-       $carro = new Carro('ABC1515', 'Branco');
-       $moto = new Moto('DEF1122', 'Preto');
-       $caminhao = new Caminhao();
-
-       $carro->trocarMarcha();
-       echo '<br />';
-       $moto->trocarMarcha();
-       echo '<br />';
-       $caminhao->trocarMarcha()
+            if($x >= 1 && $x <= 8){
+                $this->responder();
+            } else {
+                $this->executarMania();
+            }
+        }   
+    }
+    
+    $pai = new Pai();
+    
+    echo $pai->executarAcao();
 ?>
