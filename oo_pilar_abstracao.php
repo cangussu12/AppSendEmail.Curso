3a2ec2735cb4449951961ae538a3526ea4e1ccc5
cangussu12 <leo.tywin@gmail.com>
2021-11-21 23:15:18 -0300

    try - catch - finally - throw - exeption

--- oo_pilar_abstracao.php
+++ oo_pilar_abstracao.php
@@ -1,63 +1,27 @@
 <?php
 
 
-namespace A;
+//tenha uma lógica
+try {
+    //tenha uma lógica onde possa ocorrer um potencial erro ( exceção );
+    echo '<h3> *** Try *** </h3> ';
+    //$sql = 'Select * from clientes';
+    //mysql_query($sql); //erro
 
-class Cliente implements \B\CadastroInterface {
-    public $nome = 'Jorge';
-
-    public function __construct() {
-        echo '<pre>';
-        print_r(get_class_methods($this));
-        echo '</pre>';
+    if(!file_exists('require_arquivo_a.php')) {
+        throw new Error('O arquivo em questão deveria estar disponível as ' .date('d/m/Y H:i:s') . '(x horas mas não esyava. Vamos seguir mesmo assim.');
     }
 
-    public function __get($attr) {
-        return $this->$attr;
-    }
 
-    public function salvar(){
-        echo 'Salvar';
-    }
-    public function remover(){
-        echo 'Remover';
-    }
+} catch (Error $e) {
+    echo '<h3> *** Catch Error *** </h3> ';
+    echo '<p style="color: red">' . $e; '</p>';
+
+} catch (Exception $e) {
+    echo '<h3> *** Catch Excessão *** </h3>';
+    echo '<p style="color: red">' . $e; '</p>';
+} finally {
+    echo '<h3> *** Finally *** </h3>';
 }
-
-interface CadastroInterface {
-    public function salvar();
-}
-
-namespace B;
-
-class Cliente  implements \A\CadastroInterface {
-    public $nome = 'Jamilton';
-
-    public function __construct() {
-        echo '<pre>';
-        print_r(get_class_methods($this));
-        echo '</pre>';
-    }
-
-    public function __get($attr) {
-        return $this->$attr;
-    }
-
-    public function remover(){
-        echo 'Remover';
-    }
-
-    public function salvar(){
-        echo 'Salvar';
-    }
-}
-
-interface CadastroInterface {
-    public function remover();
-}
-
-$c = new Cliente();
-print_r($c);
-echo $c->__get('nome');
 
 ?>
