15e77ba7a03a18d931b0b0d3f992baa1691e1714
cangussu12 <leo.tywin@gmail.com>
2021-11-22 17:18:40 -0300

    Disparo de E-mail

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,15 @@
 <?php
 
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
+
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
 
     class Mensagem {
         private $para = null;
@@ -31,9 +41,45 @@
 
     //print_r($mensagem);
 
-    if($mensagem->mensagemValida()) {
-        echo 'Mensagem é valida';
-    } else {
+    if(!$mensagem->mensagemValida()) {
         echo 'Mensagem não é valida';
+        die();
+    } 
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'segredo@gmail.com';                     //SMTP username
+        $mail->Password   = 'secredo';                               //SMTP password
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('segredo@gmail.com', 'SendMailApp');
+        $mail->addAddress('segredo@gmail.com', 'SendMailApp');     //Add a recipient
+        $mail->addAddress('segredo@gmail.com');               //Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        //Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Oi, eu sou o assunto !';
+        $mail->Body    = '<b>Sou o body</b>';
+        $mail->AltBody = 'Sou o body';
+
+        $mail->send();
+        echo 'Não foi possível enviar este e-mail! Por favor tente novamente mais tarde.';
+    } catch (Exception $e) {
+        echo "Detalhes do erro: {$mail->ErrorInfo}";
     }
-?>
+?> 
